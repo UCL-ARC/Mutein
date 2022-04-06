dc31dddc5f4814d596c2dcc8cf73594fa520bfd7
Rachel-Alcraft <100630044+Rachel-Alcraft@users.noreply.github.com>
2022-04-06 16:41:12 +0100

    Update test_01_dummy.py

--- Pipelines/tests/test_01_dummy.py
+++ Pipelines/tests/test_01_dummy.py
@@ -1,6 +1,6 @@
 # test_dummy.py
 '''
-RSA 6/4/22
+RSA 6/4/2022
 ---------------------------
 This demonstrates a test file that is run autimatically in integration. 
 Any function thatstarts test_ in a file that startes test_ will be run automatically following a pull request.
