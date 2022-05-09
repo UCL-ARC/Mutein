16728e71b19e2ea02c0ca37cbf1d50c7e1506a0e
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-05-09 12:13:11 +0100

    Update pipeline_qsubber.py

--- Pipelines/libs/pipeline_qsubber.py
+++ Pipelines/libs/pipeline_qsubber.py
@@ -32,7 +32,7 @@ import yaml
 import sys
 
 dirs = os.path.dirname(os.path.realpath(__file__)).split("/")
-retpath = "/".join(dirs) + "/libs"
+retpath = "/".join(dirs) + ""
 sys.path.append(retpath)
 import Config
 import Paths
