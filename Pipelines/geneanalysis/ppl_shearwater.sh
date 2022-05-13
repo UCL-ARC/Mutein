5176e4cf29682f1cd9ec9a41f5c4a9ef71626617
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-05-13 21:45:06 +0100

    add shearwater

---
+++ Pipelines/geneanalysis/ppl_shearwater.sh
@@ -0,0 +1,12 @@
+### SCRIPT FOT MUTEIN PIPELINE (UCL-ARC 2022) ###
+run=qsub
+install_dir=$1
+script=${install_dir}Pipelines/libs/pipeline_qsubber.py
+config=${install_dir}Pipelines/geneanalysis/config/batch_geneprot.yml
+echo "EXE PATH=$install_dir"
+echo "CURRENT=$PWD"
+module load python3/recommended
+echo "~~~~~~~~~~~~~~~~~~ Call python script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+python ${script} $install_dir $PWD ${config} $run shearwater "" ""
+
+
--- Pipelines/libs/pipeline_qsubber.py
+++ Pipelines/libs/pipeline_qsubber.py
@@ -182,14 +182,14 @@ def pipeline_qsubber(args):
             dep = runner.run()
     # finally create the file with the error and output files for qsub
     if "qsub" in py_or_sh:
-        fdf = FileDf.FileDic(working_dir + "BatchJobs_"+dataset+"_"+gene+"_"+pdb+".csv", {})
+        fdf = FileDf.FileDic(working_dir + "/BatchJobs_"+dataset+"_"+gene+"_"+pdb+".csv", {})
         for qsubid,depid in names_and_ids:
             errorname = qsubid + ".e" + str(depid)
             outname = qsubid + ".o" + str(depid)
             fdf.add("outfile",outname)
             fdf.add("errorfile",errorname)        
         fdf.saveAsDf()
-        print("saved error and output file list to",working_dir + "BatchJobs_"+dataset+"_"+gene+"_"+pdb+".csv")
+        print("saved error and output file list to",working_dir + "/BatchJobs_"+dataset+"_"+gene+"_"+pdb+".csv")
 
 
 ####################################################################################################
