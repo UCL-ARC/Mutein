b614af4e38d285cb18690b25d6a85bc999c1ef2d
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-05-25 14:20:49 +0100

    Update remote.py

--- Pipelines/geneanalysis/python/remote.py
+++ Pipelines/geneanalysis/python/remote.py
@@ -80,8 +80,15 @@ def run_pipeline(args):
         if exists(filename):
             with open(filename, "r") as fr:
                 lines = fr.readlines()
-                for line in lines:
-                    print(line.strip())
+                for line in lines[1:]:                    
+                    geneo =ln.strip().split(",")[1]
+                    patho = Paths.Paths(DataDir,PipelineDir,dataset=dataset,gene=geneo)
+                    filenameA = patho.outputs + "ddg_background.csv"
+                    existsfile,time = checkResult(filenameA)
+                    if existsfile:
+                        print(geneo,time)
+                    else:
+                        print(geneo,"---")
         else:
             print("The dataset has not been prepared - no genes list",filename)
     elif mode == "PDBS":
