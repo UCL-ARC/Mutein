9323fb564dfd2149b16d19c2f3348862a848b0a3
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-05-25 14:51:56 +0100

    Create REMOTE_foldx_dataset_prep.sh

---
+++ Pipelines/geneanalysis/REMOTE_foldx_dataset_prep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+module load python3/recommended
+
+dataset=$1
+gene=$2
+pdb=$3
+WorkDir=$4			#/home/ucbtlcr/Scratch/workspace/
+DataDir=$5			#/home/ucbtlcr/MuteinData/
+InstallDir=$6		#/home/ucbtlcr/Mutein/
+PipelineDir=$7		#/home/ucbtlcr/Mutein/Pipelines/geneanalysis/
+
+config=${PipelineDir}config/batch_dataset_prep.yml
+script=${InstallDir}Pipelines/libs/pipeline_qsubber.py
+
+cd $DataDir
+echo "CURRENT=$PWD"
+
+python ${script} $install_dir $PWD ${config} $run $dataset "" ""
