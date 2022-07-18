d277cc4b7498a96f797773913b163dc3e5e3221a
Rachel-Alcraft <r.alcraft@ucl.ac.uk>
2022-07-18 10:30:00 +0100

    Gene stitch added

--- Pipelines/foldx/scripts/REMOTE_foldx_dataset_agg.sh
+++ Pipelines/foldx/scripts/REMOTE_foldx_dataset_agg.sh
@@ -11,13 +11,13 @@ DataDir=$5			#/home/ucbtlcr/MuteinData/
 InstallDir=$6		#/home/ucbtlcr/Mutein/
 PipelineDir=$7		#/home/ucbtlcr/Mutein/Pipelines/geneanalysis/
 
-config=${PipelineDir}config/batch_dataset_3_clean.yml
+config=${PipelineDir}config/batch_ds_2_stitch.yml
 script=${InstallDir}Pipelines/foldx/libs/pipeline_qsubber.py
 
 cd $DataDir
 echo "Submitting ${script} $InstallDir $PWD ${config} $run $dataset"
 #install_dir, working_dir, yaml_file, py_or_sh, dataset, gene, pdb
-python ${script} $InstallDir $DataDir ${config} $run $dataset "x" "x"
+python ${script} $InstallDir $DataDir ${config} $run $dataset $gene "x"
 
 
  
