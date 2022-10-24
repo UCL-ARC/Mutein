522519e063576fb0f2bc308bc02ca2986a1616b8
Rob Vickerstaff <r.vickerstaff@ucl.ac.uk>
2022-10-24 16:57:49 +0100

    fix rebuild script

--- Pipelines/scripts/rebuild_container.sh
+++ Pipelines/scripts/rebuild_container.sh
@@ -3,4 +3,4 @@
 #export PATH=${PATH}:/opt/singularity/bin
 export DEFFILE=/home/ccaervi/Mutein/Pipelines/singularity/mutein.def
 
-sudo /opt/singularity/bin/singularity build --allow-setuid mutein.sif ${DEFFILE}
+sudo /opt/singularity/bin/singularity build mutein.sif ${DEFFILE}
