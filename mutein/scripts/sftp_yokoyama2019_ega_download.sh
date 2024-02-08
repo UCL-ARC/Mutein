624dc51bb0eec7d911a441dd2f07e4768eb48275
Rob Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-02-08 10:16:32 +0000

    added sftp download script for yokoyama2019 ega download

---
+++ mutein/scripts/sftp_yokoyama2019_ega_download.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# download yokoyama from ega outbox using sftp
+#
+# -r recursively copy
+# -a attempt to continue interrupted transfers
+# -F path to ssh config file
+#
+# to be run in /SAN/medic/Mutein/549_mutein/datasets/yokoyama2019
+#
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+sftp -ra -F ./ssh_config ega-outbox 2> ${timestamp}.err > ${timestamp}.out <<EOF
+get EGAD00001004462
+get EGAD00001004464
+get EGAD00001001631
+bye
+EOF
+
+
