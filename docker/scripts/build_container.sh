286cc05cb46e221a3dc89aaf71f0262edbc925b4
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-03-15 11:46:01 +0000

    docker build now works, no longer needs github access key

--- docker/scripts/build_container.sh
+++ docker/scripts/build_container.sh
@@ -3,6 +3,7 @@
 #
 # script used to build the docker container image on the docker build VM
 # run from the directory containing the Dockerfile
+#
 
 set -eu
 
@@ -12,14 +13,6 @@ if [[ "$#" != "0" ]] ; then
     echo "$usage"
     exit
 fi
-
-#copy the github keys into the build context
-mkdir -p secrets
-chmod 0700 secrets
-cp /vagrant/untracked/id_ed25519 secrets/id_ed25519
-chmod 0600 secrets/id_ed25519
-cp /vagrant/untracked/id_ed25519.pub secrets/id_ed25519.pub
-chmod 0644 secrets/id_ed25519.pub
 
 docker build -t mutein_tre .
 docker image list
